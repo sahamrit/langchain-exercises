b08bcb75dbf5c046651158939c56e924eea449e7
sahamrit <amritsahu.iitkgp@gmail.com>
2024-05-22 13:03:18 +0530

    serve using langserve

---
+++ translate/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from typing import List
+
+from fastapi import FastAPI
+from langserve import add_routes
+
+from translate_chain import chain
+
+# App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# Adding chain route
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
