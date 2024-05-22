db3b20adb34346a459ab76c13e6b4317e12038de
sahamrit <amritsahu.iitkgp@gmail.com>
2024-05-22 12:41:48 +0530

    langsmith works

---
+++ translate_app.py
@@ -0,0 +1,14 @@
+import os
+
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4")
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage(content="Translate the following from English into Italian"),
+    HumanMessage(content="hi!"),
+]
+
+model.invoke(messages)
